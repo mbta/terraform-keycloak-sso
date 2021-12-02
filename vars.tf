74bf151c380508bf07a6f85cd6d642c87dc419e5
Ian Westcott <iwestcott@mbta.com>
2021-12-01 22:23:23 -0500

    clean up vars

--- vars.tf
+++ vars.tf
@@ -3,6 +3,11 @@ variable "acm_certificate_arn" {
   description = "(optional) ARN of certificate for load balancer to use. Defaults to auto-creating a new certificate"
   default = null
 }
+
+variable "availability_zones" {
+  description = "List of availability zones"
+}
+
 variable "aws_region" {
   type        = string
   description = "AWS Region"
@@ -12,45 +17,6 @@ variable "database_subnet_group" {
   type = string
   description = "(optional) Name of a database subnet group where Keycloak MariaDB instance will reside. Defaults to creating a new subnet group containing var.private_subnets"
   default = null
-}
-
-variable "ecr_keycloak_image_url" {
-  type        = string
-  description = "(optional) ECR Keycloak Image location. Defaults to creating a new ECR repo"
-  default = null
-}
-variable "ecs_cluster_arn" {
-  type = string
-  description = "(optional) ARN of an existing ECS cluster. Defaults to creating a new ECS cluster"
-}
-
-variable "environment" {
-  type = string
-  description = "Name of environment, e.g. 'dev', 'prod'"
-}
-
-variable "log_bucket_owner_id" {
-  type        = string
-  description = "IAM id for the logs bucket owner (differs per region)"
-}
-
-variable "public_subnets" {
-  type = list(string)
-  description = "List of public subnets in the VPC where Keycloak will reside"
-}
-
-variable "private_subnets" {
-  type = list(string)
-  description = "List of private subnets in the VPC where Keycloak will reside"
-}
-
-variable "vpc_id" {
-  type = string
-  description = "ID of an existing VPC where Keycloak will reside"
-}
-
-variable "availability_zones" {
-  description = "List of availability zones"
 }
 
 variable "db_name" {
@@ -63,9 +29,20 @@ variable "db_username" {
   description = "Keycloak database username"
 }
 
-variable "db_password" {
+variable "ecr_keycloak_image_url" {
   type        = string
-  description = "Keycloak database user password"
+  description = "(optional) ECR Keycloak Image location. Defaults to creating a new ECR repo"
+  default = null
+}
+
+variable "ecs_cluster_arn" {
+  type = string
+  description = "(optional) ARN of an existing ECS cluster. Defaults to creating a new ECS cluster"
+}
+
+variable "environment" {
+  type = string
+  description = "Name of environment, e.g. 'dev', 'prod'"
 }
 
 variable "kc_username" {
@@ -73,7 +50,22 @@ variable "kc_username" {
   description = "Keycloak admin username"
 }
 
-variable "kc_password" {
+variable "log_bucket_owner_id" {
   type        = string
-  description = "Keycloak admin user password"
+  description = "IAM id for the logs bucket owner (differs per region)"
+}
+
+variable "private_subnets" {
+  type = list(string)
+  description = "List of private subnets in the VPC where Keycloak will reside"
+}
+
+variable "public_subnets" {
+  type = list(string)
+  description = "List of public subnets in the VPC where Keycloak will reside"
+}
+
+variable "vpc_id" {
+  type = string
+  description = "ID of an existing VPC where Keycloak will reside"
 }
