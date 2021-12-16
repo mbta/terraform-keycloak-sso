7c2edf17b803d92379ccd3b47313184e6249666e
Ian Westcott <iwestcott@mbta.com>
2021-12-15 22:43:45 -0500

    fix: add IAM policy for secrets manager access

--- iam.tf
+++ iam.tf
@@ -44,3 +44,21 @@ resource "aws_iam_role" "keycloak-db-monitoring-role" {
   tags = var.tags
 }
 
+# inline policy for Secrets Manager access, attached to the task execution role created by the ECS module
+resource "aws_iam_role_policy" "inline-keycloak-secretsmanager" {
+  name   = "secretsmanager-keycloak-${var.environment}"
+  role   = aws_iam_role.keycloak-ecs-execution-task-role.name
+  policy = data.aws_iam_policy_document.inline-keycloak-secretsmanager-doc.json
+}
+
+# iam policy document for inline-keycloak-secretsmanager above
+data "aws_iam_policy_document" "inline-keycloak-secretsmanager-doc" {
+  statement {
+    actions = ["secretsmanager:GetSecretValue"]
+    effect  = "Allow"
+    resources = [
+      aws_secretsmanager_secret.keycloak-admin-password.arn,
+      aws_secretsmanager_secret.keycloak-database-password.arn,
+    ]
+  }
+}
