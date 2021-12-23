eb2c2366f77a6416164384c73058fefd16491f13
Ian Westcott <iwestcott@mbta.com>
2021-12-23 16:43:40 -0500

    feat: allow sending email via SES

---
+++ ses.tf
@@ -0,0 +1,19 @@
+data "aws_iam_policy_document" "keycloak-ses-sender" {
+  statement {
+    actions   = ["ses:SendRawEmail", "ses:SendEmail"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "keycloak-ses-sender" {
+  name        = "keycloak-ses-sender"
+  description = "Allows sending of e-mails via Simple Email Service"
+  policy      = data.aws_iam_policy_document.keycloak-ses-sender.json
+}
+
+# Attach to ECS task role
+resource "aws_iam_role_policy_attachment" "keycloak-ses-send-mail-policy-attachment" {
+  role       = aws_iam_role.keycloak-ecs-execution-task-role.name
+  policy_arn = aws_iam_policy.keycloak-ses-sender.arn
+}
+
