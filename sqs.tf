a0b0cbdfb8f3b2dfecffab1a7e7896d075e14b52
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2023-03-10 14:51:47 -0500

    feat (WIP): Keycloak to T-Alerts user updates SQS queue
    Encrypt the queue.
    
    Allow Keycloak to publish to this queue. This will be followed by a PR
    in devops to grant alerts_concierge read permission.

---
+++ sqs.tf
@@ -0,0 +1,26 @@
+resource "aws_sqs_queue" "keycloak_to_alerts_concierge_user_updates" {
+  name = "keycloak-${var.environment}-alerts-concierge-user-updates"
+  sqs_managed_sse_enabled = true
+}
+
+# allow Keycloak to publish messages to SQS
+resource "aws_sqs_queue_policy" "keycloak_to_alerts_concierge_user_updates" {
+  queue_url = aws_sqs_queue.keycloak_to_alerts_concierge_user_updates.id
+  policy    = data.aws_iam_policy_document.keycloak_to_alerts_concierge_user_updates.json
+}
+resource "aws_iam_role_policy" "keycloak_to_alerts_concierge_user_updates_publish" {
+  name   = "keycloak-${var.environment}-alerts-concierge-user-updates-publish"
+  role   = resource.keycloak-service.keycloak-service.id
+  policy = data.aws_iam_policy_document.keycloak_to_alerts_concierge_user_updates_publish.json
+}
+
+data "aws_iam_policy_document" "keycloak_to_alerts_concierge_user_updates_publish" {
+  statement {
+    sid = "AllowSendFromKeycloak"
+    effect = "Allow"
+    actions = ["sqs:SendMessage"]
+    resources = [
+      aws_sqs_queue.keycloak_to_alerts_concierge_user_updates.arn
+    ]
+  }
+}
