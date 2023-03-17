5fb06b4a3d271a16c45db8bdadf6847acd84434e
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2023-03-17 10:27:09 -0400

    refactor: Move the IAM policy and doc to the iam file

--- iam.tf
+++ iam.tf
@@ -69,3 +69,24 @@ data "aws_iam_policy_document" "inline-keycloak-secretsmanager-doc" {
     )
   }
 }
+
+# allow Keycloak to publish messages to SQS
+resource "aws_iam_role_policy" "keycloak_to_alerts_concierge_user_updates_publish" {
+  name   = "keycloak-${var.environment}-alerts-concierge-user-updates-publish"
+  role   = resource.keycloak-service.keycloak-service.id
+  policy = data.aws_iam_policy_document.keycloak_to_alerts_concierge_user_updates_publish.json
+}
+
+data "aws_iam_policy_document" "keycloak_to_alerts_concierge_user_updates_publish" {
+  statement {
+    sid = "AllowSendFromKeycloak"
+    effect = "Allow"
+    actions = [
+      "sqs:GetQueueUrl",
+      "sqs:SendMessage"
+    ]
+    resources = [
+      aws_sqs_queue.keycloak_to_alerts_concierge_user_updates.arn
+    ]
+  }
+}
--- sqs.tf
+++ sqs.tf
@@ -2,24 +2,3 @@ resource "aws_sqs_queue" "keycloak_to_alerts_concierge_user_updates" {
   name = "keycloak-${var.environment}-alerts-concierge-user-updates"
   sqs_managed_sse_enabled = true
 }
-
-# allow Keycloak to publish messages to SQS
-resource "aws_iam_role_policy" "keycloak_to_alerts_concierge_user_updates_publish" {
-  name   = "keycloak-${var.environment}-alerts-concierge-user-updates-publish"
-  role   = resource.keycloak-service.keycloak-service.id
-  policy = data.aws_iam_policy_document.keycloak_to_alerts_concierge_user_updates_publish.json
-}
-
-data "aws_iam_policy_document" "keycloak_to_alerts_concierge_user_updates_publish" {
-  statement {
-    sid = "AllowSendFromKeycloak"
-    effect = "Allow"
-    actions = [
-      "sqs:GetQueueUrl",
-      "sqs:SendMessage"
-    ]
-    resources = [
-      aws_sqs_queue.keycloak_to_alerts_concierge_user_updates.arn
-    ]
-  }
-}
