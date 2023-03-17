13dae2025298a07ed42235c8fd08022a22e71042
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2023-03-17 17:01:56 -0400

    fix: Output the arn value of the SQS queue

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,7 @@ output "alb_endpoint" {
 output "alb_zone_id" {
   value = aws_alb.keycloak-load-balancer.zone_id
 }
+
+output "sqs_queue_arn" {
+  value = aws_sqs_queue.keycloak_to_alerts_concierge_user_updates.arn
+}
